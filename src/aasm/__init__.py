79cde922e1572056e34498f0a37f7009ae6465cd
halthinks <halthinks@gmail.com>
2026-08-08 00:13:27 -0700

    AASM v0.9: export control center economics and executors

--- src/aasm/__init__.py
+++ src/aasm/__init__.py
@@ -1,24 +1,29 @@
-from .runtime_v08 import AASMEngine
+from .runtime_v09 import AASMEngine
 from .model import MachineState, ProblemSpec, TaskEnvelope, Proposal, Result, CapabilitySet
 from .agents import AASMAgent, FunctionAgent
 from .authority import SingleControllerAuthority, AutonomousAuthority, QuorumAuthority, HierarchicalAuthority
 from .effects import EffectSpec, EffectRecord, EffectStatus, RetryPolicy, EffectExecutionError, EffectUnknownOutcome
 from .definitions import MachineDefinition, default_machine_definition
 from .model_check import ModelCheckIssue, ModelCheckReport, check_machine
-from .persistence import MemoryStore, SQLiteStore
+from .persistence import MemoryStore, SQLiteStore, PostgresStore
 from .workers import WorkerRecord, WorkerStatus, TaskLease, LeaseStatus, QuotaPolicy
 from .model_routing import ModelProfile, ModelRouteRequest, ModelRouteResult, ModelStrengthRouter
 from .remote import AASMRemoteClient, RemoteProtocolError
 from .worker_loop import RemoteWorkerLoop
+from .economics import CallPurpose, ModelPricing, ModelUsageRecord, ReviewGatePolicy, EconomicsLedger
+from .openai_executor import OpenAIResponsesExecutor, OpenAIExecutionResult, OpenAIExecutorError
+from .codex_executor import CodexCLIExecutor, CodexExecutionResult, CodexExecutorError
 
 __all__=[
     "AASMEngine","MachineState","ProblemSpec","TaskEnvelope","Proposal","Result","CapabilitySet",
     "AASMAgent","FunctionAgent","SingleControllerAuthority","AutonomousAuthority","QuorumAuthority","HierarchicalAuthority",
-    "MemoryStore","SQLiteStore","EffectSpec","EffectRecord","EffectStatus","RetryPolicy","EffectExecutionError","EffectUnknownOutcome",
+    "MemoryStore","SQLiteStore","PostgresStore","EffectSpec","EffectRecord","EffectStatus","RetryPolicy","EffectExecutionError","EffectUnknownOutcome",
     "MachineDefinition","default_machine_definition","ModelCheckIssue","ModelCheckReport","check_machine",
     "ResourceRecord","TaskDemand","Assignment","ScheduleResult","CapabilityScheduler",
     "WorkerRecord","WorkerStatus","TaskLease","LeaseStatus","QuotaPolicy",
-    "ModelProfile","ModelRouteRequest","ModelRouteResult","ModelStrengthRouter","AASMRemoteClient","RemoteProtocolError","RemoteWorkerLoop"
+    "ModelProfile","ModelRouteRequest","ModelRouteResult","ModelStrengthRouter","AASMRemoteClient","RemoteProtocolError","RemoteWorkerLoop",
+    "CallPurpose","ModelPricing","ModelUsageRecord","ReviewGatePolicy","EconomicsLedger",
+    "OpenAIResponsesExecutor","OpenAIExecutionResult","OpenAIExecutorError","CodexCLIExecutor","CodexExecutionResult","CodexExecutorError"
 ]
 
 from .graph import PlanNode, PlanEdge, PlanGraph
