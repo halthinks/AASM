777d64036e66faf646d809588985ead98306d3be
halthinks <halthinks@gmail.com>
2026-08-08 00:13:40 -0700

    AASM v0.9: export Postgres store

--- src/aasm/persistence/__init__.py
+++ src/aasm/persistence/__init__.py
@@ -1,6 +1,7 @@
 from .base import Store
 from .memory import MemoryStore
 from .sqlite import SQLiteStore
+from .postgres import PostgresStore
 from .factory import open_store
 
-__all__ = ["Store", "MemoryStore", "SQLiteStore", "open_store"]
+__all__ = ["Store", "MemoryStore", "SQLiteStore", "PostgresStore", "open_store"]
