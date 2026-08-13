d7556b4cf6b962ec7a4899d6545a5af6067c88a3
halthinks <halthinks@gmail.com>
2026-08-13 10:39:59 -0700

    Add v0.41 runtime

---
+++ src/aasm/runtime_v41.py
@@ -0,0 +1,11 @@
+from .runtime_v40 import AASMEngine as V40Engine
+from ._runtime_v41_reuse_records import ReuseRecordRuntimeMixin
+from ._runtime_v41_reuse_refs import ReuseReferenceRuntimeMixin
+from ._runtime_v41_reuse_lookup import ReuseLookupRuntimeMixin
+from ._runtime_v41_reuse_certify import ReuseCertificationRuntimeMixin
+from ._runtime_v41_reuse_commit import ReuseCommitRuntimeMixin
+from ._runtime_v41_metrics import ReuseMetricsRuntimeMixin
+from ._runtime_v41_solver import SolverLoopRuntimeMixin
+
+class AASMEngine(SolverLoopRuntimeMixin,ReuseCommitRuntimeMixin,ReuseCertificationRuntimeMixin,ReuseMetricsRuntimeMixin,ReuseLookupRuntimeMixin,ReuseReferenceRuntimeMixin,ReuseRecordRuntimeMixin,V40Engine):
+    pass
