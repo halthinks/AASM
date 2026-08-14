31ee041de8387e44ae25d03d79359220dcc00d3a
halthinks <halthinks@gmail.com>
2026-08-14 12:23:27 -0700

    Activate v0.50 public surface

--- src/aasm/__init__.py
+++ src/aasm/__init__.py
@@ -1,2 +1,2 @@
-from .public_v49 import *
-from .public_v49 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
+from .public_v50 import *
+from .public_v50 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
