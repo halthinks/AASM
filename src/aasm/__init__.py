c4efbab8e11e852261cd04222e3aea56388b9919
halthinks <halthinks@gmail.com>
2026-08-14 02:46:56 -0700

    Activate v0.46 public API

--- src/aasm/__init__.py
+++ src/aasm/__init__.py
@@ -1,2 +1,2 @@
-from .public_v45 import *
-from .public_v45 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
+from .public_v46 import *
+from .public_v46 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
