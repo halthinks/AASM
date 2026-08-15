7b90b2ab84542255e9cd275f4bba120d3a586c3d
halthinks <halthinks@gmail.com>
2026-08-14 20:18:37 -0700

    Activate v0.52 public runtime

--- src/aasm/__init__.py
+++ src/aasm/__init__.py
@@ -1,2 +1,2 @@
-from .public_v51 import *
-from .public_v51 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
+from .public_v52 import *
+from .public_v52 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
