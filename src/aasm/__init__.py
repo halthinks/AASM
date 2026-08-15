42e9277fbf4db8054e2ccbf6b15856eede8822da
halthinks <halthinks@gmail.com>
2026-08-15 15:07:38 -0700

    Activate public v0.55 root surface

--- src/aasm/__init__.py
+++ src/aasm/__init__.py
@@ -1,2 +1,2 @@
-from .public_v54 import *
-from .public_v54 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
+from .public_v55 import *
+from .public_v55 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
