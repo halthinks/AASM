387efb585e4c78ed44594f49dc53d20809dadc53
halthinks <halthinks@gmail.com>
2026-08-16 12:00:01 -0700

    Promote active public overlay for PR-3 capability and fencing admission

--- src/aasm/__init__.py
+++ src/aasm/__init__.py
@@ -1,2 +1,2 @@
-from .public_v56 import *
-from .public_v56 import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
+from .public_active import *
+from .public_active import __version__, AASMEngine, validate_public_api_contract, public_api_contract, PUBLIC_API_CONTRACT
